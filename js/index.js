4687715179b2841a575e3d7584cd0b13ed725da6
Tate Keller <tatek1993@gmail.com>
2020-01-14 14:12:45 -0600

    added click, dblclick, mouseenter and mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const wiggleBus = document.querySelector(".intro img")
+// console.log(wiggleBus);
+
+//mouse enter - 1
+wiggleBus.addEventListener("mouseenter", () => {
+    wiggleBus.style.transform = "scale(1.5)";
+    wiggleBus.style.transition = "transform 0.2s"
+})
+
+//mouse leave - 2
+wiggleBus.addEventListener("mouseleave", () => {
+    wiggleBus.style.transform = "scale(1)";
+    
+})
+
+const logoHeading = document.querySelector(".logo-heading");
+
+//click - 3
+logoHeading.addEventListener("click", () =>{
+    logoHeading.textContent = "Eyy Honk Honk, Baby!"
+})
+//double click - 4
+logoHeading.addEventListener("dblclick", () => {
+    logoHeading.textContent = "What, you don't like honking?"
+})
+
